9ea6c14b3d1272ca0878761cf5e4a46c6dde47e0
almog27 <almog27@users.noreply.github.com>
2021-03-17 19:53:23 +0000

    deploy: 4785282e0a6c176599f3307342fa21ea115f8c4b

---
+++ _nuxt/static/1616010802/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fgenerated-nuxt\u002F_nuxt\u002Fstatic\u002F1616010802",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{app:{basePath:"\u002Fgenerated-nuxt\u002F",assetsPath:"\u002Fgenerated-nuxt\u002F_nuxt\u002F",cdnURL:a}}}}(null));
