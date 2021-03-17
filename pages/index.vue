1c7dfca3f2f08fab5560ab829d8a4fa089bb0c90
Almog Ben-David <almog.bendavid@snyk.io>
2021-03-17 21:34:16 +0200

    feat: deploy command to gh pages

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,7 @@
   <div class="container">
     <div>
       <Logo />
-      <h1 class="title">generated-nuxt</h1>
+      <h1 class="title">my-generated-nuxt</h1>
       <div class="links">
         <a
           href="https://nuxtjs.org/"
